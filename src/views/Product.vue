f1b2b7c0187d4661690a9cc7e0fce61d01045441
Xucong ZHAN <zhanxucong@gmail.com>
2019-02-19 10:48:46 -0500

    Added idividual product page

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,14 +1,40 @@
 <template>
   <div>
     <section class="wrapper">
-      <h2>{{ product.name }}</h2>
+      <div class="flex-col">
+        <img :src="makeImagePath(product)" alt class="flex-col--2">
+        <div class="flex-col--2">
+          <h2>{{ product.name }}</h2>
+          <p>Price: ${{ product.price }}</p>
+          <p v-if="typeof product.size === 'string'">Size: {{ product.size }}</p>
+          <div v-if="typeof product.size === 'object'">
+            <p>Size:</p>
+            <p>&nbsp;&nbsp;Waist: {{product.size.waist}}</p>
+            <p>&nbsp;&nbsp;Length: {{product.size.length}}</p>
+          </div>
+          <p>Color: {{ product.color }}</p>
+          <p>
+            <em>{{ product.quantity }} left in stock</em>
+          </p>
+          <h3>Details</h3>
+          <ul>
+            <li>Material: {{ product.details.material }}</li>
+            <li>Fit: {{ product.details.fit }}</li>
+            <li>Maintenance: {{ product.details.maintenance }}</li>
+            <li v-if="product.details.additional">Additional: {{ product.details.additional }}</li>
+          </ul>
+        </div>
+      </div>
     </section>
   </div>
 </template>
 
 <script>
+import { imagePath } from "@/mixins/imagePath.js";
+
 export default {
   name: "product",
+  mixins: [imagePath],
   data() {
     return {
       product: this.$store.getters.product(this.$route.params.id)
@@ -17,4 +43,17 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.flex-col {
+  display: flex;
+  align-items: flex-start;
+}
+
+.flex-col--2 {
+  width: 50%;
+}
+
+li {
+  line-height: 150%;
+}
+</style>
